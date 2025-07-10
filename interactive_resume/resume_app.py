25cd19b698d94d811bb4caa5d53487423bd85e56
JoeyFromDataPunk <r.joseph.boyle@gmail.com>
2025-07-10 13:57:38 -0400

    Restore headshot in app layout

--- interactive_resume/resume_app.py
+++ interactive_resume/resume_app.py
@@ -6,7 +6,7 @@ col1, col2 = st.columns([1, 4])  # Adjust ratio as needed
 # Left column: Image
 with col1:
     st.write("")
-    # st.image("jb_headshot.jpg", width=150)
+    st.image("jb_headshot.jpg", width=150)
 
 # Title and subtitle
 with col2:
